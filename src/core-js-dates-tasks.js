9a097b2d6eca409f0895cf08681f54178ea10ea9
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-03-26 02:53:20 +0500

    feat: implement getDayName, getNextFriday, getCountDaysInMonth functions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -51,8 +51,18 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const dayNames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  return dayNames[new Date(date).getUTCDay()];
 }
 
 /**
@@ -66,8 +76,14 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const resultDate = new Date(date.getTime());
+  const daysToAdd = (5 - resultDate.getDay() + 7) % 7;
+  const daysUntilNextFriday = daysToAdd === 0 ? 7 : daysToAdd;
+
+  resultDate.setDate(resultDate.getDate() + daysUntilNextFriday);
+
+  return resultDate;
 }
 
 /**
@@ -81,8 +97,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
