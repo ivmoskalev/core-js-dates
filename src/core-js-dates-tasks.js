673d69bfd348744fd76f5b7586c523cbee51e206
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-03-26 03:35:21 +0500

    feat: implement getCoutDaysOnPeriod, isDateInPeriod, formatDate functions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -112,8 +112,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+  return (end - start) / (1000 * 60 * 60 * 24) + 1;
 }
 
 /**
@@ -133,8 +135,11 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const dateObj = new Date(date);
+  const start = new Date(period.start);
+  const end = new Date(period.end);
+  return dateObj >= start && dateObj <= end;
 }
 
 /**
@@ -148,8 +153,24 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const dateObj = new Date(date);
+
+  const month = dateObj.getUTCMonth() + 1;
+  const day = dateObj.getUTCDate();
+  const year = dateObj.getUTCFullYear();
+  let hour = dateObj.getUTCHours();
+  const minute = dateObj.getUTCMinutes();
+  const second = dateObj.getUTCSeconds();
+
+  const amPm = hour >= 12 ? 'PM' : 'AM';
+  hour %= 12;
+  hour = hour || 12;
+
+  const paddedMinute = minute < 10 ? `0${minute}` : minute;
+  const paddedSecond = second < 10 ? `0${second}` : second;
+
+  return `${month}/${day}/${year}, ${hour}:${paddedMinute}:${paddedSecond} ${amPm}`;
 }
 
 /**
